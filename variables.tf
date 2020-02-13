e0af31d0131aefbbc8eee8ae84dc6f56be7cc1ea
ayusmadi <57968900+ayusmadi@users.noreply.github.com>
2020-02-13 08:40:00 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -41,3 +41,8 @@ variable "custom_data" {
   description = "Specifies custom data to supply to the machine. On Linux-based systems, this can be used as a cloud-init script."
   default     = "apt_upgrade: true"
 }
+
+variable "delete_disks_on_termination" {
+  description = "Delete all disks on when virtual machine is deleted"
+  default     = false
+}
