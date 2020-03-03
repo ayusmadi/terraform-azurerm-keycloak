68a4baa8cd812854105436134a327f0a954f2fe2
ayusmadi <57968900+ayusmadi@users.noreply.github.com>
2020-03-03 09:00:34 +0800

    Add outputs for admin_username and fqdn (#8)

--- outputs.tf
+++ outputs.tf
@@ -8,8 +8,18 @@ output "public_ip_address" {
   description = "Public IP address associated with the network interface"
 }
 
+output "admin_username" {
+  value       = var.admin_username
+  description = "Username to access the Windows virtual machine"
+}
+
 output "admin_password" {
   value       = random_password.password.result
   description = "Password to access the Linux virtual machine"
   sensitive   = true
+}
+
+output "fqdn" {
+  value = azurerm_public_ip.main.fqdn
+  description = "Fully qualified domain name of the A DNS record associated with the public IP. domain_name_label must be specified to get the fqdn. This is the concatenation of the domain_name_label and the regionalized DNS zone"
 }
